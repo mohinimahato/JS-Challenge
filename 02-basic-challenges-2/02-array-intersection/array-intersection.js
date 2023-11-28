3d235fcbe866601436b33cc0cb74e2edd2350888
Mohini Mahato <mohinimahatowork@gmail.com>
2023-11-28 15:57:52 +0530

    Array Intersection Done

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    const intArr = [];
+    for (let i = 0; i <= arr1.length; i++) {
+        if (arr2.includes(arr1[i]) && !intArr.includes(arr1[i])) {
+            intArr.push(arr1[i]);
+        }
+    }
+    return intArr;
+}
 
 module.exports = arrayIntersection;
