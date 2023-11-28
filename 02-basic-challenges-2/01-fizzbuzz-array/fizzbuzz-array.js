5b1e508cdc74cce26f938f1afff839882ddf3485
Mohini Mahato <mohinimahatowork@gmail.com>
2023-11-28 15:41:19 +0530

    Fizzbuzz done

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,20 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const arr = []
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            arr.push("FizzBuzz")
+        } else if (i % 5 === 0) {
+            arr.push("Buzz");
+        }
+        else if (i % 3 === 0) {
+            arr.push("Fizz");
+        }
+        else {
+            arr.push(i)
+        }
 
+    }
+    return arr;
+}
+console.log(fizzBuzzArray(15))
 module.exports = fizzBuzzArray;
